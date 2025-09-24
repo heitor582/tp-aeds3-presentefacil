84cb9644b5b5dbc8013be2378de1bba06896e5cc
heitor582 <heitorvieira.pvd@gmail.com>
2025-09-24 20:46:18 -0300

    sla

--- src/presentefacil/repository/user/IdEmailIndexPair.java
+++ src/presentefacil/repository/user/IdEmailIndexPair.java
@@ -7,38 +7,48 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 import repository.ExtensibleHashContract;
+import shared.StringValidate;
 
 public final class IdEmailIndexPair implements ExtensibleHashContract {
     private int id = -1;
     private String email = "";
     private short SIZE = 30;
 
-    public IdEmailIndexPair(){};
-    
-    private IdEmailIndexPair(final int id, final String email){
+    public IdEmailIndexPair() {
+    };
+
+    private IdEmailIndexPair(final int id, final String email) {
         this.id = id;
         this.email = email;
     }
 
-    public static IdEmailIndexPair create(final int id, final String email){
+    public static IdEmailIndexPair create(final int id, final String email) {
+        StringValidate.requireNonBlank(email);
         return new IdEmailIndexPair(id, email);
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return this.email;
     }
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
 
     @Override
-    public short size(){
+    public short size() {
         return this.SIZE;
     }
 
     @Override
-    public byte[] toByteArray() throws IOException{
+    public String toString() {
+        return String.format(
+                "ID: %d | Email: %s | Size: %d",
+                id, email, SIZE);
+    }
+
+    @Override
+    public byte[] toByteArray() throws IOException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         DataOutputStream dos = new DataOutputStream(baos);
         dos.writeInt(this.id);
@@ -47,7 +57,7 @@ public final class IdEmailIndexPair implements ExtensibleHashContract {
     }
 
     @Override
-    public void fromByteArray(final byte[] ba) throws IOException{
+    public void fromByteArray(final byte[] ba) throws IOException {
         ByteArrayInputStream bais = new ByteArrayInputStream(ba);
         DataInputStream dis = new DataInputStream(bais);
         this.id = dis.readInt();
